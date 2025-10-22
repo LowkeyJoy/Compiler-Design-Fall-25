293f4a0e8cabec2df080cdcec94aa3834e1ed720
LowkeyJoy <ahmedjoy1002@gmail.com>
2025-10-22 14:36:31 +0600

    Add files via upload
    Lab 1 - qustion 1

---
+++ COMPILER.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isNum(string s) {
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] < 48 || s[i] > 57) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string input;
+
+    cout << "enter a number: ";
+    cin >> input;
+
+    if (isNum(input)) {
+        cout << "numeric constant" << endl;
+    } else {
+        cout << "not  numeric constant" << endl;
+    }
+
+    return 0;
+}
